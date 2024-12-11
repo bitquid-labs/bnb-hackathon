1426bfc97ca12097b82f0d1f510c41ed07830d22
tosynthegeek <oluwatosinojedapo@gmail.com>
2024-12-11 16:57:52 +0100

    feat: added ERC20 abi for token transfers

--- canisters/src/icp_backend/src/util.rs
+++ canisters/src/icp_backend/src/util.rs
@@ -1,4 +1,8 @@
-use alloy::hex;
+use alloy::{
+    hex,
+    signers::icp::IcpSigner,
+    transports::icp::{RpcApi, RpcService},
+};
 use hex::FromHexError;
 
 pub fn to_hex(data: &[u8]) -> String {
@@ -7,4 +11,26 @@ pub fn to_hex(data: &[u8]) -> String {
 
 pub fn from_hex(data: &str) -> Result<Vec<u8>, FromHexError> {
     hex::decode(&data[2..])
+}
+
+fn get_ecdsa_key_name() -> String {
+    #[allow(clippy::option_env_unwrap)]
+    let dfx_network = option_env!("DFX_NETWORK").unwrap();
+    match dfx_network {
+        "local" => "dfx_test_key".to_string(),
+        "ic" => "key_1".to_string(),
+        _ => panic!("Unsupported network."),
+    }
+}
+
+pub async fn create_icp_signer() -> IcpSigner {
+    let ecdsa_key_name = get_ecdsa_key_name();
+    IcpSigner::new(vec![], &ecdsa_key_name, None).await.unwrap()
+}
+
+pub fn generate_rpc_service(rpc_url: String) -> RpcService {
+    RpcService::Custom(RpcApi {
+        url: rpc_url,
+        headers: None,
+    })
 }
